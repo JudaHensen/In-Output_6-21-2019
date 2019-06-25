e9114d568f7273d5329b8cb7c4a3dec2f4b4e224
JudaHensen <24311@ma-web.nl>
2019-06-25 12:30:11 +0200

    Images can be uploaded, need to create a userinterface soon.

--- Project/upload_image.js
+++ Project/upload_image.js
@@ -1,14 +1,33 @@
 class UploadImage {
 
-  constructor(ctx) {
-    this.ctx = ctx
+  constructor(context, fileInput) {
+    // this.ctx = context;
+    this.input = fileInput;
+
+    this.input.addEventListener('change', this.Upload, false);
   }
 
-  SetImage(parent) {
-    this.image = parent.files[0];
+  // Get image
+  Upload() {
+    // Check if file can be found
+    if ( this.files && this.files[0] ) {
+      // create a file reader to read the file
+      let reader = new FileReader();
+      reader.readAsDataURL( this.files[0] );
 
-    this.ctx.drawImage(this.image, 100, 100);
-    console.log('kek');
+      reader.onload = function(e) {
+        let img = new Image();
+        img.src = e.target.result;
+
+        // draw image on canvas when loaded
+        img.addEventListener("load", function() {
+          canvas.width = img.naturalWidth;
+          canvas.height = img.naturalHeight;
+
+          context.drawImage(img, 0, 0);
+        });
+      };
+    }
   }
 
 }
